8ec6bba3a0d6c2acd486e437a536d0c2140076b6
Sewer 56 <seweratwork@gmail.com>
2022-11-02 17:00:59 +0000

    Added: AWB Emulator Config, Build, CI/CD

--- PublishAll.ps1
+++ PublishAll.ps1
@@ -2,13 +2,20 @@
 Split-Path $MyInvocation.MyCommand.Path | Push-Location
 [Environment]::CurrentDirectory = $PWD
 
-
 ./Publish.ps1 -ProjectPath "Emulator/AFS.Stream.Emulator/AFS.Stream.Emulator.csproj" `
               -PackageName "AFS.Stream.Emulator" `
               -PublishOutputDir "Publish/ToUpload/AFS" `
               -MakeDelta false -UseGitHubDelta false `
 			  -ReadmePath "docs/emulators/afs.md" `
 			  -ChangelogPath "Emulator/AFS.Stream.Emulator/CHANGELOG.MD" `
+              -GitHubUserName Sewer56 -GitHubRepoName Sewer56 -GitHubInheritVersionFromTag false
+			  
+./Publish.ps1 -ProjectPath "Emulator/AWB.Stream.Emulator/AWB.Stream.Emulator.csproj" `
+              -PackageName "AWB.Stream.Emulator" `
+              -PublishOutputDir "Publish/ToUpload/AWB" `
+              -MakeDelta false -UseGitHubDelta false `
+			  -ReadmePath "docs/emulators/awb.md" `
+			  -ChangelogPath "Emulator/AWB.Stream.Emulator/CHANGELOG.MD" `
               -GitHubUserName Sewer56 -GitHubRepoName Sewer56 -GitHubInheritVersionFromTag false
 
 ./Publish.ps1 -ProjectPath "Emulator/ONE.Heroes.Stream.Emulator/ONE.Heroes.Stream.Emulator.csproj" `
