5666684fd0aa0bfaeb6b6de7d1559cc8550593d9
Sewer 56 <seweratwork@gmail.com>
2022-11-02 16:24:23 +0000

    Changed: Use Thread Local Buffer for Symlink Resolution

--- FileEmulationFramework/Utilities/FilePathResolver.cs
+++ FileEmulationFramework/Utilities/FilePathResolver.cs
@@ -9,6 +9,12 @@ namespace FileEmulationFramework.Utilities;
 public static unsafe class FilePathResolver
 {
     private const short MaxPath = short.MaxValue; // Windows 10 with path extension.
+    
+    [ThreadStatic]
+    private static char[]? _pathBuffer;
+    
+    // by putting in pinned object heap, pinning is a no-op below
+    private static char[] GetPathBuffer() => _pathBuffer ??= GC.AllocateUninitializedArray<char>(MaxPath, true);
 
     [DllImport("Kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
     static extern uint GetFinalPathNameByHandleW(IntPtr hFile, void* data, uint cchFilePath, uint dwFlags);
@@ -20,17 +26,19 @@ public static unsafe class FilePathResolver
     /// <param name="result">Resulting path name.</param>
     public static bool TryGetFinalPathName(IntPtr handle, out string result)
     {
-        var buffer = stackalloc char[MaxPath];
-        var res = GetFinalPathNameByHandleW(handle, buffer, (uint)MaxPath, 0);
-        if (res == 0)
+        fixed (char* buffer = GetPathBuffer())
         {
-            result = "";
-            return false;
-        }
+            var res = GetFinalPathNameByHandleW(handle, buffer, (uint)MaxPath, 0);
+            if (res == 0)
+            {
+                result = "";
+                return false;
+            }
 
-        // Use GetFullPath to normalize returned path.
-        result = RemoveDevicePrefix(new ReadOnlySpan<char>(buffer, (int)res));
-        return true;
+            // Use GetFullPath to normalize returned path.
+            result = RemoveDevicePrefix(new ReadOnlySpan<char>(buffer, (int)res));
+            return true;
+        }
     }
 
     private static string RemoveDevicePrefix(ReadOnlySpan<char> path)
